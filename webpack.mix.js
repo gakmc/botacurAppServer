59e04beff79d416ccc948cafaa463e6bb2667a8e
Gabriel <gak335@gmail.com>
2024-12-09 17:13:03 -0300

    Cambios

--- webpack.mix.js
+++ webpack.mix.js
@@ -4,19 +4,23 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
    .sass('resources/sass/app.scss', 'public/css')
-   .sass('resources/sass/403.scss', 'public/css')
-   .browserSync({
-      proxy: 'botacura.com',
-      socket: {
-         clients: false,
-      },
-      files: [
-         'app/**/*.php',
-         'resources/views/**/*.php',
-         'public/js/**/*.js',
-         'public/css/**/*.css',
-      ]
-  });
+   .sass('resources/sass/403.scss', 'public/css');
+ 
+ 
+ 
+ 
+//    .browserSync({
+//       proxy: 'botacura.com',
+//       socket: {
+//          clients: true,
+//       },
+//       files: [
+//          'app/**/*.php',
+//          'resources/views/**/*.php',
+//          'public/js/**/*.js',
+//          'public/css/**/*.css',
+//       ]
+//   });
   
   // .browserSync({
   //    proxy: 'botacura.com',
